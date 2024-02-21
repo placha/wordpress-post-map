e2a3336ef32aceffa0c56ad725015f714f213a44
Kacper Placha <34598604+placha@users.noreply.github.com>
2024-02-21 21:40:37 +0100

    Update plugin update uri

--- index.php
+++ index.php
@@ -2,14 +2,14 @@
 /**
  * WordPress Post Map
  *
- * @package           placha/wordpress-map
+ * @package           placha/wordpress-post-map
  * @author            Kacper Placha
  * @copyright         2024 Kacper Placha
  * @license           MIT
  *
  * @wordpress-plugin
  * Plugin Name:       WordPress Post Map
- * Plugin URI:        https://placha.pl/wordpress-post-map
+ * Plugin URI:        https://github.com/placha/wordpress-post-map
  * Description:       Generates geojson file from portfolio posts (meta: coordinates) and provide shortcode: [post-map]
  * Version:           1.0.0
  * Requires at least: 6.0
@@ -18,7 +18,7 @@
  * Author URI:        https://placha.pl/
  * Text Domain:       wordpress-post-map
  * License:           MIT
- * Update URI:        https://placha.pl/wordpress-post-map/update
+ * Update URI:        https://raw.githubusercontent.com/placha/wordpress-post-map/main/current.json
  */
 
 function main()
@@ -28,9 +28,10 @@ function main()
 add_shortcode('post-map', 'postMapShortcode');
 function postMapShortcode($attr): string
 {
-	$plugin_data = get_plugin_data( __FILE__ );
+    $plugin_data = get_plugin_data( __FILE__ );
     $result = do_shortcode('[leaflet-map zoom="12" lat="50.06172102288047" lng="19.93735195760001" height="500" width="100%" min_zoom="8" max_zoom="16" zoomcontrol fitbounds !tap]');
     $result .= do_shortcode('[leaflet-geojson src=' . get_site_url() . '/wp-content/plugins/'.$plugin_data['TextDomain'].'/geojson-render.php]{popup-text}[/leaflet-geojson]');
+
     return $result;
 }
 
